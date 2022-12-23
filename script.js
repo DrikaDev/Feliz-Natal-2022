cafeac2d570f0d78e248747466e16cb6040c92d0
drikadev <adriguti.adriana@hotmail.com>
2022-12-23 17:24:49 -0300

    enviando arquivos do projeto pronto

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayAbout(){
+    let arrow = document.getElementById('animation');
+        arrow.classList.toggle('rotate');
+    let display = document.getElementById('about');
+        display.classList.toggle('active');
+}
